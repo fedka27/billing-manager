056ffac9d56bd39b038506615c72ebebfe0b459b
Fedya Trubitsyn <trubitsyn.fedya@yandex.ru>
2018-04-18 17:00:15 +0200

    [+] Lib Billing Manager empty initialized;

---
+++ app/src/main/java/com/github/fedka27/lib_billing_manager/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.github.fedka27.lib_billing_manager
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
